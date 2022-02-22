dcbd0e9ab8101a6cc80a88467832385b1cd2c607
Wilson Fernandes <wilson.fcj@aluno.ifsc.edu.br>
2022-02-22 19:57:30 -0300

    Enviando infos

--- app/src/main/java/br/com/tabajara/continuousscrollable/MainActivity.kt
+++ app/src/main/java/br/com/tabajara/continuousscrollable/MainActivity.kt
@@ -1,11 +1,19 @@
 package br.com.tabajara.continuousscrollable
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSprit = findViewById<Button>(R.id.btn_sprit)
+        btnSprit?.setOnClickListener {
+            val intent = Intent(applicationContext, SpriteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/br/com/tabajara/continuousscrollable/SpriteActivity.kt
@@ -0,0 +1,13 @@
+package br.com.tabajara.continuousscrollable
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SpriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sprite)
+    }
+}
